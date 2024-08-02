36cf1cf11e3925e4a8a5a5399915aafe60d15604
Precha Krasaesup <main@Prechas-Mac-Pro.local>
2024-08-03 02:18:37 +0700

    feat: add skelton loading

--- components/Home/CarsList.tsx
+++ components/Home/CarsList.tsx
@@ -1,24 +1,28 @@
 import React, { useState } from "react";
 import CarCard from "./CarCard";
 import BookingModal from "../CarBooking/BookingModal";
+import CarCardSkelton from "./CarCardSkelton";
 
 function CarsList(props: any) {
+  const [isLoaded, setIsLoaded] = useState(true);
   const [selectedCar, setSelectedCar] = useState<any>([]);
   return (
     <div className="grid grid-cols-2 min-[325px]:grid-cols-1 md:grid-cols-3 lg:grid-cols-4">
-      {props.carsList.map((car: any, index: number) => (
-        <div
-          key={index}
-          onClick={() => {
-            (window as any).my_modal_4.showModal();
-            setSelectedCar(car);
-          }}
-        >
-          <CarCard car={car} />
-        </div>
-      ))}
-      {/* You can open the modal using document.getElementById('ID').showModal() method */}
-
+      {isLoaded &&
+        props.carsList.map((car: any, index: number) => (
+          <div
+            key={index}
+            onClick={() => {
+              (window as any).my_modal_4.showModal();
+              setSelectedCar(car);
+            }}
+          >
+            <CarCard car={car} />
+          </div>
+        ))}
+      {isLoaded
+        ? [1, 2, 3, 4, 5, 6, 7, 8].map((item) => <CarCardSkelton />)
+        : null}
       <dialog id="my_modal_4" className="modal">
         <BookingModal selectedCar={selectedCar} />
       </dialog>
