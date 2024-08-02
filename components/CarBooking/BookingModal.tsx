c958e717e1b4d3d8d8ae6694e0012207e68b234e
Precha Krasaesup <main@Prechas-Mac-Pro.local>
2024-08-02 17:39:02 +0700

    feat: implement car modal form

--- components/CarBooking/BookingModal.tsx
+++ components/CarBooking/BookingModal.tsx
@@ -1,17 +1,27 @@
 import React from "react";
+import CarCard from "../Home/CarCard";
+import Form from "./Form";
 
-function BookingModal() {
+function BookingModal({ selectedCar }: any) {
   return (
-    <div className="modal-box w-11/12 max-w-5xl">
-      <h3 className="font-bold text-lg">Hello!</h3>
-      <p className="py-4">Click the button below to close</p>
-      <div className="modal-action">
-        <form method="dialog">
-          {/* if there is a button, it will close the modal */}
-          <button className="btn">Close</button>
-        </form>
+    <form method="dialog" className="modal-box w-11/12 max-w-5xl">
+      <div className="border-b-[1px] pb-2 ">
+        <h3 className=" text-[30px] font-light text-gray-400">
+          Rent A Car Now!
+        </h3>
       </div>
-    </div>
+      <div
+        className="grid grid-cols-1
+    md:grid-cols-2 "
+      >
+        <div>
+          <CarCard car={selectedCar} />
+        </div>
+        <div>
+          <Form />
+        </div>
+      </div>
+    </form>
   );
 }
 
--- components/Home/CarsList.tsx
+++ components/Home/CarsList.tsx
@@ -1,19 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import CarCard from "./CarCard";
 import BookingModal from "../CarBooking/BookingModal";
 
 function CarsList(props: any) {
+  const [selectedCar, setSelectedCar] = useState<any>([]);
   return (
     <div className="grid grid-cols-2 min-[325px]:grid-cols-1 md:grid-cols-3 lg:grid-cols-4">
       {props.carsList.map((car: any, index: number) => (
-        <div key={index} onClick={() => (window as any).my_modal_4.showModal()}>
+        <div
+          key={index}
+          onClick={() => {
+            (window as any).my_modal_4.showModal();
+            setSelectedCar(car);
+          }}
+        >
           <CarCard car={car} />
         </div>
       ))}
       {/* You can open the modal using document.getElementById('ID').showModal() method */}
 
       <dialog id="my_modal_4" className="modal">
-        <BookingModal />
+        <BookingModal selectedCar={selectedCar} />
       </dialog>
     </div>
   );
